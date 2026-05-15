c893e3a04a9c64e506f6037b43bec9976b755341
Evgeny Leshok <work.evles@gmail.com>
2026-05-15 18:23:33 +0300

    feat: update import service wiring

--- backend/src/main/kotlin/yayauheny/by/di/ServiceModule.kt
+++ backend/src/main/kotlin/yayauheny/by/di/ServiceModule.kt
@@ -4,9 +4,14 @@ import org.koin.dsl.module
 import yayauheny.by.service.CityService
 import yayauheny.by.service.CountryService
 import yayauheny.by.service.RestroomService
+import yayauheny.by.service.import.schedule.ScheduleAdapter
+import yayauheny.by.service.import.schedule.ScheduleMappingService
+import yayauheny.by.service.import.schedule.TwoGisScheduleAdapter
 
 val serviceModule =
     module {
+        single<ScheduleAdapter> { TwoGisScheduleAdapter() }
+        single { ScheduleMappingService(getAll()) }
         single<CountryService> { CountryService(get()) }
         single<CityService> { CityService(get(), get()) }
         single<RestroomService> { RestroomService(get()) }
